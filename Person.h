2e1600ccb94030b21c2b34cb309c00b556309ae7
karimtamer112 <karimtamer112@gmail.com>
2025-11-11 09:54:12 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,25 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <iostream>
 #include <string>
+
 using namespace std;
 
-//#write Person class here
+class Person{
 
+    private:
+        
+        string name;
+        int id;
+    
+    public:
+        
+        Person(string name, int id);
+        Person();
+        ~Person();
+        void display();
 
-
-
-
-
-
-
-
-
-
+};
 
 #endif
